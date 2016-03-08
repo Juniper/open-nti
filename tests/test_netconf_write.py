d06819ddc732fdeb2283c2c6730fbca439d9bcba
Michael Pergament <mpergament@googlemail.com>
2016-03-08 15:17:59 +0100

    Additional tests + different OS support

--- tests/test_netconf_write.py
+++ tests/test_netconf_write.py
@@ -1,32 +1,44 @@
 from docker import Client
+import influxdb
 import docker.tls as tls
 from os import path
 import pprint
 import subprocess
 import sys, getopt, argparse, os.path, math
+from sys import platform as _platform
 
-# dockerout = subprocess.check_output(
-#     ['/usr/local/bin/docker-machine ip default'],
-#     shell=True, stderr=subprocess.STDOUT
-# )
-#
-# dockerip = dockerout.splitlines()[0]
-#
-# CERTS = path.join(path.expanduser('~'), '.docker', 'machine', 'machines', 'default')
-#
-# tls_config = tls.TLSConfig(
-#     client_cert=(path.join(CERTS, 'cert.pem'), path.join(CERTS, 'key.pem')),
-#     ca_cert=path.join(CERTS, 'ca.pem'),
-#     assert_hostname=False,
-#     verify=True
-# )
-#
-# url = "https://" + dockerip + ":2376"
-# print "URL: " + url
-# c = Client(base_url= url, tls=tls_config)
+if _platform == "linux" or _platform == "linux2":
+    # linux
+    c = Client(base_url='unix://var/run/docker.sock', version='1.20')
+elif _platform == "darwin":
+    # MAC OS X
+    dockerout = subprocess.check_output(
+        ['/usr/local/bin/docker-machine ip default'],
+        shell=True, stderr=subprocess.STDOUT
+    )
 
-c = Client(base_url='unix://var/run/docker.sock', version='1.20')
+    dockerip = dockerout.splitlines()[0]
+
+    CERTS = path.join(path.expanduser('~'), '.docker', 'machine', 'machines', 'default')
+
+    tls_config = tls.TLSConfig(
+        client_cert=(path.join(CERTS, 'cert.pem'), path.join(CERTS, 'key.pem')),
+        ca_cert=path.join(CERTS, 'ca.pem'),
+        assert_hostname=False,
+        verify=True
+    )
+
+    url = "https://" + dockerip + ":2376"
+    print "URL: " + url
+    c = Client(base_url= url, tls=tls_config)
+elif _platform == "win32":
+    exit
 
 def test_connect_docker():
     containers = c.containers()
     assert len(containers) == 1
+
+def test_influxdb_running_database_exist():
+    db = influxdb.InfluxDBClient("127.0.0.1", "8086", "juniper", "juniper", "juniper")
+    db_list = db.get_list_database()
+    assert len(db_list) == 2
