265c8fef712e098dbe08eddc1a866ec6c4e72cc1
Michael Pergament <mpergament@googlemail.com>
2016-03-08 14:12:48 +0100

    Change script to run on Linux

--- tests/test_netconf_write.py
+++ tests/test_netconf_write.py
@@ -5,25 +5,26 @@ import pprint
 import subprocess
 import sys, getopt, argparse, os.path, math
 
-dockerout = subprocess.check_output(
-    ['/usr/local/bin/docker-machine ip default'],
-    shell=True, stderr=subprocess.STDOUT
-)
-
-dockerip = dockerout.splitlines()[0]
-
-CERTS = path.join(path.expanduser('~'), '.docker', 'machine', 'machines', 'default')
-
-tls_config = tls.TLSConfig(
-    client_cert=(path.join(CERTS, 'cert.pem'), path.join(CERTS, 'key.pem')),
-    ca_cert=path.join(CERTS, 'ca.pem'),
-    assert_hostname=False,
-    verify=True
-)
-
-url = "https://" + dockerip + ":2376"
-print "URL: " + url
-c = Client(base_url= url, tls=tls_config)
+# dockerout = subprocess.check_output(
+#     ['/usr/local/bin/docker-machine ip default'],
+#     shell=True, stderr=subprocess.STDOUT
+# )
+#
+# dockerip = dockerout.splitlines()[0]
+#
+# CERTS = path.join(path.expanduser('~'), '.docker', 'machine', 'machines', 'default')
+#
+# tls_config = tls.TLSConfig(
+#     client_cert=(path.join(CERTS, 'cert.pem'), path.join(CERTS, 'key.pem')),
+#     ca_cert=path.join(CERTS, 'ca.pem'),
+#     assert_hostname=False,
+#     verify=True
+# )
+#
+# url = "https://" + dockerip + ":2376"
+# print "URL: " + url
+# c = Client(base_url= url, tls=tls_config)
+c = Client(base_url='unix://var/run/docker.sock')
 
 def test_connect_docker():
     containers = c.containers()
