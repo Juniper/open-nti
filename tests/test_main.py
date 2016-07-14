1fc4fcd598b8385a27c11c4bd125bd5b5d306726
Damien <dgarros@gmail.com>
2016-07-14 12:08:21 +0200

    Refactor tests for multi-containers

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,9 +17,25 @@ pp = pprint.PrettyPrinter(indent=4)
 ############################################
 # Variables initialization
 #############################################
-# Image and container
-IMAGE_NAME = 'juniper/open-nti'
-CONTAINER_NAME = 'open-nti_test'
+# Images and containers
+
+OPENNTI_NAME = 'open-nti_test'
+OPENNTI_IMAGE = 'juniper/open-nti'
+OPENNTI_C = ''
+OPENNTI_IP = ''
+
+OPENNTI_IN_JTI_NAME = 'open-nti_in_jti_test'
+OPENNTI_IN_JTI_IMAGE = 'juniper/open-nti-input-jti'
+OPENNTI_IN_JTI_C = ''
+
+OPENNTI_IN_LOG_NAME = 'open-nti_in_log_test'
+OPENNTI_IN_LOG_IMAGE = 'juniper/open-nti-input-syslog'
+OPENNTI_IN_LOG_C = ''
+
+TCP_REPLAY_IMAGE = 'dgarros/tcpreplay'
+TCP_REPLAY_C = ''
+TCP_REPLAY_NAME = 'tcpreplay_test'
+
 DATABASE_NAME = 'juniper'
 GRAFANA_USER = 'admin'
 GRAFANA_PASS = 'admin'
@@ -27,13 +43,13 @@ GRAFANA_PASS = 'admin'
 # Local ports that will be redirected to the Open NTI
 # Startup will fail if some ports are already in use
 TEST_PORT_JTI = 40000
-TEST_PORT_NA = 40010
 TEST_PORT_ANALYTICSD = 40020
 TEST_PORT_STATSD = 48125
 TEST_PORT_EVENT = 46000
 TEST_PORT_GRAFANA = 43000
 TEST_PORT_INFLUXDB = 48083
 TEST_PORT_INFLUXDB_API = 48086
+INFLUXDB_PORT = 8086
 
 # Local directories that will be mapped into the container
 CURRENT_DIR = os.getcwd()
@@ -47,10 +63,8 @@ LOCAL_GRAFANA_DIR = LOCAL_DIR_TESTS + '/grafana'
 LOCAL_GRAFANA_FILE = LOCAL_GRAFANA_DIR + '/test.json'
 
 DOCKER_IP = '127.0.0.1'
-CONTAINER_ID = ''
-TCP_REPLAY_CONTAINER_ID = ''
-HANDLE_DB = ''
 
+HANDLE_DB = ''
 
 #############################################
 def get_handle_db():
@@ -67,49 +81,44 @@ def get_handle_db():
 
     return HANDLE_DB
 
-
 #############################################
 class StreamLineBuildGenerator(object):
     def __init__(self, json_data):
         self.__dict__ = json.loads(json_data)
-
-# def setup_function(function):
-#     print('\nsetup_function()')
-
 
 def test_connect_docker():
     global c
     global DOCKER_IP
 
     # initialize Docker Object
-    if _platform == "linux" or _platform == "linux2":
+    if _platform == "linux" or _platform == "linux2" or _platform == "darwin":
         # linux
         c = Client(base_url='unix://var/run/docker.sock', version='1.20')
-    elif _platform == "darwin":
-        # MAC OS X
-        dockerout = subprocess.check_output(
-            ['/usr/local/bin/docker-machine ip default'],
-            shell=True, stderr=subprocess.STDOUT
-        )
-
-        DOCKER_IP = dockerout.splitlines()[0]
-
-        CERTS = path.join(
-            path.expanduser('~'), '.docker', 'machine',
-            'machines', 'default'
-        )
-
-        tls_config = tls.TLSConfig(
-            client_cert=(
-                path.join(CERTS, 'cert.pem'), path.join(CERTS, 'key.pem')
-            ),
-            ca_cert=path.join(CERTS, 'ca.pem'),
-            assert_hostname=False,
-            verify=True
-        )
-
-        url = "https://" + DOCKER_IP + ":2376"
-        c = Client(base_url=url, tls=tls_config, version='1.20')
+    # elif _platform == "darwin":
+    #     # MAC OS X
+    #     dockerout = subprocess.check_output(
+    #         ['/usr/local/bin/docker-machine ip default'],
+    #         shell=True, stderr=subprocess.STDOUT
+    #     )
+    #
+    #     DOCKER_IP = dockerout.splitlines()[0]
+    #
+    #     CERTS = path.join(
+    #         path.expanduser('~'), '.docker', 'machine',
+    #         'machines', 'default'
+    #     )
+    #
+    #     tls_config = tls.TLSConfig(
+    #         client_cert=(
+    #             path.join(CERTS, 'cert.pem'), path.join(CERTS, 'key.pem')
+    #         ),
+    #         ca_cert=path.join(CERTS, 'ca.pem'),
+    #         assert_hostname=False,
+    #         verify=True
+    #     )
+    #
+    #     url = "https://" + DOCKER_IP + ":2376"
+    #     c = Client(base_url=url, tls=tls_config, version='1.20')
 
     elif _platform == "win32":
         exit
@@ -118,24 +127,23 @@ def test_connect_docker():
     list_images = c.images()
     assert len(list_images) >= 1
 
-
-def test_start_container():
-    global CONTAINER_ID
+def test_start_opennti_container():
+    global OPENNTI_C
+    global OPENNTI_IP
 
     # Force Stop and delete existing container if exist
     try:
-        old_container_id = c.inspect_container(CONTAINER_NAME)['Id']
-
+        old_container_id = c.inspect_container(OPENNTI_NAME)['Id']
         c.stop(container=old_container_id)
         c.remove_container(container=old_container_id)
     except:
         print "Container do not exit"
 
-    # Create new container
+    # Create new  container
     container = c.create_container(
-        image=IMAGE_NAME,
+        image=OPENNTI_IMAGE,
         command='/sbin/my_init',
-        name=CONTAINER_NAME,
+        name=OPENNTI_NAME,
         detach=True,
         ports=[
             (8125, 'udp'),
@@ -155,10 +163,6 @@ def test_start_container():
         host_config=c.create_host_config(
             port_bindings={
                 '8125/udp': TEST_PORT_STATSD,
-                '6000/udp': TEST_PORT_EVENT,
-                '50000/udp': TEST_PORT_JTI,
-                '50010/udp': TEST_PORT_NA,
-                '50020/udp': TEST_PORT_ANALYTICSD,
                 3000: TEST_PORT_GRAFANA,
                 8083: TEST_PORT_INFLUXDB,
                 8086: TEST_PORT_INFLUXDB_API
@@ -172,14 +176,106 @@ def test_start_container():
     )
 
     c.start(container)
-
-    CONTAINER_ID = c.inspect_container(CONTAINER_NAME)['Id']
+    OPENNTI_C   = c.inspect_container(OPENNTI_NAME)['Id']
+    OPENNTI_IP  = c.inspect_container(OPENNTI_NAME)['NetworkSettings']['IPAddress']
 
     # Wait few sec for the container to start
     time.sleep(10)
 
-    assert c.inspect_container(CONTAINER_NAME)["State"]["Running"]
+    assert c.inspect_container(OPENNTI_NAME)["State"]["Running"]
 
+def test_start_opennti_in_jti_container():
+    global OPENNTI_IN_JTI_C
+
+    # Force Stop and delete existing container if exist
+    try:
+        old_container_id = c.inspect_container(OPENNTI_IN_JTI_NAME)['Id']
+        c.stop(container=old_container_id)
+        c.remove_container(container=old_container_id)
+    except:
+        print "Container do not exit"
+
+    # Create new  container
+    container = c.create_container(
+        image=OPENNTI_IN_JTI_IMAGE,
+        name=OPENNTI_IN_JTI_NAME,
+        detach=True,
+        ports=[
+            (50000, 'udp'),
+            (50020, 'udp')
+        ],
+        environment=[
+            "PORT_JTI="+str(TEST_PORT_JTI),
+            "PORT_ANALYTICSD="+str(TEST_PORT_ANALYTICSD),
+            "INFLUXDB_PORT="+str(INFLUXDB_PORT),
+            "INFLUXDB_DB="+str(DATABASE_NAME),
+            "INFLUXDB_USER=juniper",
+            "INFLUXDB_PWD=juniper",
+            "INFLUXDB_ADDR="+OPENNTI_IP,
+            "INFLUXDB_FLUSH_INTERVAL=1"
+            "OUTPUT_INFLUXDB=true",
+            "OUTPUT_STDOUT=true"
+        ],
+        host_config=c.create_host_config(
+            port_bindings={
+                str(TEST_PORT_JTI)+'/udp': TEST_PORT_JTI,
+                str(TEST_PORT_ANALYTICSD)+'/udp': TEST_PORT_ANALYTICSD
+            }
+        )
+    )
+
+    c.start(container)
+    OPENNTI_IN_JTI_C = c.inspect_container(OPENNTI_IN_JTI_NAME)['Id']
+
+    # Wait few sec for the container to start
+    time.sleep(1)
+
+    assert c.inspect_container(OPENNTI_IN_JTI_NAME)["State"]["Running"]
+
+def test_start_opennti_in_log_container():
+    global OPENNTI_IN_LOG_C
+    global OPENNTI_IP
+
+    # Force Stop and delete existing container if exist
+    try:
+        old_container_id = c.inspect_container(OPENNTI_IN_LOG_NAME)['Id']
+        c.stop(container=old_container_id)
+        c.remove_container(container=old_container_id)
+    except:
+        print "Container do not exit"
+
+    # Create new  container
+    container = c.create_container(
+        image=OPENNTI_IN_LOG_IMAGE,
+        name=OPENNTI_IN_LOG_NAME,
+        detach=True,
+        ports=[
+            (TEST_PORT_EVENT, 'udp')
+        ],
+        environment=[
+            "INFLUXDB_PORT="+str(INFLUXDB_PORT),
+            "INFLUXDB_DB="+str(DATABASE_NAME),
+            "INFLUXDB_USER=juniper",
+            "INFLUXDB_PWD=juniper",
+            "INFLUXDB_ADDR="+OPENNTI_IP,
+            "PORT_SYSLOG="+str(TEST_PORT_EVENT),
+            "OUTPUT_INFLUXDB=true",
+            "OUTPUT_STDOUT=true"
+        ],
+        host_config=c.create_host_config(
+            port_bindings={
+                str(TEST_PORT_EVENT)+'/udp': TEST_PORT_EVENT
+            }
+        )
+    )
+
+    c.start(container)
+    OPENNTI_IN_LOG_C = c.inspect_container(OPENNTI_IN_LOG_NAME)['Id']
+
+    # Wait few sec for the container to start
+    time.sleep(1)
+
+    assert c.inspect_container(OPENNTI_IN_LOG_NAME)["State"]["Running"]
 
 def test_influxdb_running_database_exist():
     # Verify we can connect to InfluxDB and DB with a name juniper exists
@@ -202,12 +298,12 @@ def test_influxdb_running_database_exist():
 
 def test_collection_agent_01():
     # Write datapoint using mocked Junos device
-    global CONTAINER_ID
+    global OPENNTI_C
 
     FIXTURES_DIR = "/opt/open-nti/tests/fixtures/test_collection_agent_01/"
 
     exec_job_id = c.exec_create(
-        container=CONTAINER_ID,
+        container=OPENNTI_C,
         cmd='/usr/bin/python ' +
         '/opt/open-nti/open-nti.py -s -t --tag test --input ' + FIXTURES_DIR
     )
@@ -236,33 +332,40 @@ def test_collection_agent_01():
 
 
 def test_start_tcpreplay_container():
-    global TCP_REPLAY_CONTAINER_ID
+    global TCP_REPLAY_C
 
-    # TODO add tear down of existing container if exist
+
+    # Force Stop and delete existing container if exist
+    try:
+        old_container_id = c.inspect_container(TCP_REPLAY_NAME)['Id']
+        c.stop(container=old_container_id)
+        c.remove_container(container=old_container_id)
+    except:
+        print "Container do not exit"
 
     container = c.create_container(
-        image='dgarros/tcpreplay',
+        image=TCP_REPLAY_IMAGE,
         command='/usr/bin/tcpreplay --intf1=eth0 /data/test50000.pcap',
-        name='tcpreplay',
+        name=TCP_REPLAY_NAME,
         detach=True,
         volumes=[
             '/data'
         ],
         host_config=c.create_host_config(
             binds=[
-                LOCAL_PCAP_DIR + ':/data',
+                LOCAL_PCAP_DIR + ':/data'
             ]
         )
     )
 
     c.start(container)
 
-    TCP_REPLAY_CONTAINER_ID = c.inspect_container('tcpreplay')['Id']
+    TCP_REPLAY_C = c.inspect_container(TCP_REPLAY_NAME)['Id']
 
     # Wait few sec for the container to start
-    time.sleep(30)
+    time.sleep(20)
 
-    assert c.inspect_container('tcpreplay')['Name'] == '/tcpreplay'
+    assert c.inspect_container(TCP_REPLAY_NAME)["State"]["Running"]
 
 
 def test_jti_agent():
@@ -293,12 +396,20 @@ def test_grafana_running():
 
     assert 1
 
-
 def teardown_module(module):
     global c
-    global CONTAINER_ID
+    global OPENNTI_C
+    global OPENNTI_IN_JTI_C
+    global OPENNTI_IN_LOG_C
 
-    c.stop(container=CONTAINER_ID)
-    c.remove_container(container=CONTAINER_ID)
-    c.stop(container=TCP_REPLAY_CONTAINER_ID)
-    c.remove_container(container=TCP_REPLAY_CONTAINER_ID)
+    c.stop(container=OPENNTI_C)
+    c.remove_container(container=OPENNTI_C)
+
+    c.stop(container=OPENNTI_IN_JTI_C)
+    c.remove_container(container=OPENNTI_IN_JTI_C)
+
+    c.stop(container=OPENNTI_IN_LOG_C)
+    c.remove_container(container=OPENNTI_IN_LOG_C)
+
+    c.stop(container=TCP_REPLAY_C)
+    c.remove_container(container=TCP_REPLAY_C)
