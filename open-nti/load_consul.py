c550fc353dad8c4eb906b5e7e972c2e77b624548
Damien Garros <dgarros@juniper.net>
2016-12-22 08:57:10 -0800

    Load 'hosts', 'credentials', 'commands', 'variables' YAML files to Consul

--- open-nti/load_consul.py
+++ open-nti/load_consul.py
@@ -5,6 +5,7 @@ import os
 import requests
 import httplib2
 import json
+import yaml
 
 # logger = logging.getLogger( 'input' )
 
@@ -24,8 +25,34 @@ if __name__ == "__main__":
                 commands_file = BASE_DIR + '/../data/commands.yaml'
             )
 
+    ############################################################
+    ## Load all files in JSON format
+    ############################################################
+    for file in ['hosts', 'credentials', 'commands', 'variables']:
+
+      ## TODO Add check to verify that YAML is valid
+      file_addr = BASE_DIR + '/../data/{0}.yaml'.format(file)
+
+      file_yaml = []
+      with open(file_addr) as f:
+        for document in yaml.load_all(f):
+          file_yaml.append(document)
+
+    #   file_content = open(file_addr).read()
+    #   file_yaml = yaml.load_all(file_content)
+
+      url = "http://localhost:8500/v1/kv/parameters/{0}".format(file)
+
+      r = requests.put(
+        url,
+        data=json.dumps(file_yaml[0]),
+      )
+
+    ############################################################
+    ## Load parameters for each Hosts and Inputs
+    ############################################################
     hosts = Input.get_target_hosts()
-    print hosts
+    # print hosts
     # print Input.get_target_commands(host='192.168.194.128')
 
     for host in hosts:
@@ -34,20 +61,30 @@ if __name__ == "__main__":
         ## Upload list of commands to Consul
         commands = Input.get_target_commands(host=host, type=type)
 
+        ## Load commands
         if not commands == []:
           url = "http://localhost:8500/v1/kv/inputs/{0}/{1}/commands".format(type, host)
-          headers = {}
           r = requests.put(
             url,
             data=json.dumps(commands),
           )
 
+        ## Load tags
+        tags = Input.get_tags(host=host)
+        url = "http://localhost:8500/v1/kv/inputs/{0}/{1}/tags".format(type, host)
+        r = requests.put(
+            url,
+            data=json.dumps(tags),
+          )
+
+        ## Load credentials
         if type == 'netconf':
           url = "http://localhost:8500/v1/kv/inputs/{0}/{1}/credential".format(type, host)
           # print url
           credential = Input.get_target_credential(host=host)
-          headers = {}
           r = requests.put(
             url,
             data=json.dumps(credential),
           )
+
+    ## Load general Parameters
