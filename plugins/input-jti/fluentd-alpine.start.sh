ab8a115439848dde3aa5a5a12df5136a5f5d4fcd
Damien Garros <dgarros@juniper.net>
2016-12-23 11:02:39 -0800

    Add consul agent in input-jti container

--- plugins/input-jti/fluentd-alpine.start.sh
+++ plugins/input-jti/fluentd-alpine.start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# `/sbin/setuser memcache` runs the given command as the user `memcache`.
-# If you omit that part, the command will be run as root.
+
+/usr/local/bin/consul agent -config-dir /etc/consul/conf.d >>/var/log/consul.log 2>&1 &
 
 envtpl --keep-template /fluentd/etc/fluent.conf -o /tmp/fluent.conf
 
