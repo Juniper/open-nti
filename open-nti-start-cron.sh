babafc4e1e36ea99e4934af782021ea53edd01b5
Damien Garros <dgarros@juniper.net>
2017-01-03 11:54:01 -0800

    Update open-nti-*-cron.sh scripts with new container name variable and added a message to indicate deprecation.

--- open-nti-start-cron.sh
+++ open-nti-start-cron.sh
@@ -2,7 +2,8 @@
 
 #Load params file with all variables
 source ./open-nti.params
+echo "$(tput setaf 5)this script (open-nti-start-cron.sh) is deprecated, it's been replaced with 'make cron-add' $(tput sgr0)"
 
 time=$1
 comd=$2
-docker exec -it $CONTAINER_NAME /usr/bin/python /opt/open-nti/startcron.py -a add -t "$time" -c "/usr/bin/python /opt/open-nti/open-nti.py -s $comd"
+docker exec -it $MAIN_CONTAINER_NAME /usr/bin/python /opt/open-nti/startcron.py -a add -t "$time" -c "/usr/bin/python /opt/open-nti/open-nti.py -s $comd"
