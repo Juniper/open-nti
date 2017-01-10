8602f898ab244be41fae6b094f6706e2353939d4
Damien Garros <dgarros@juniper.net>
2017-01-10 09:22:48 -0800

    Add telegraf in input-syslog container for perf-monitoring

--- plugins/input-syslog/fluentd-alpine.start.sh
+++ plugins/input-syslog/fluentd-alpine.start.sh
@@ -2,6 +2,8 @@
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
 
+telegraf --config /home/fluent/telegraf.toml >>/var/log/telegraf-monitoring.log 2>&1 &
+
 envtpl --keep-template /fluentd/etc/fluent.conf -o /tmp/fluent.conf
 
 fluentd -c /tmp/fluent.conf -p /fluentd/plugins $FLUENTD_OPT
