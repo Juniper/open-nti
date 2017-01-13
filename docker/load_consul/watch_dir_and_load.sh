32ff31bb220f87be5fa92a008c761aed647f6578
Damien Garros <dgarros@juniper.net>
2017-01-13 15:58:51 -0800

    Fix load consul script to logs proper output

--- docker/load_consul/watch_dir_and_load.sh
+++ docker/load_consul/watch_dir_and_load.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-alias load_yaml='/usr/bin/python load_consul.py --dir=/opt/open-nti/data'
+alias load_yaml='/usr/bin/python load_consul.py --dir=/opt/open-nti/data >> /var/log/load_consul.log 2>&1'
 cd /opt/load_consul
-load_yaml; fswatch -o /opt/open-nti/data | while read f; do load_yaml; done ; done >>/var/log/load_consul.log 2>&1
+load_yaml; fswatch -o /opt/open-nti/data | while read f; do load_yaml; done ; done
