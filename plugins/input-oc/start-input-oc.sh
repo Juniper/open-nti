8404f9c6c2a9926bc36665664dad9a14cb917f02
psagrera <psagrera@juniper.net>
2017-11-27 03:23:23 -0800

    adding Open Config plugin to the master branch

---
+++ plugins/input-oc/start-input-oc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mkdir -p /opt/telegraf/config
+opennti=$(cat /etc/hosts | grep "opennti " | cut -f1)
+sed s/opennti/$opennti/g /source/telegraf.tmpl > /opt/telegraf/config/telegraf.conf
+/go/src/github.com/influxdata/telegraf/telegraf --config /opt/telegraf/config/telegraf.conf
+#/go/bin/telegraf --config /opt/telegraf/config/telegraf.conf 
+
