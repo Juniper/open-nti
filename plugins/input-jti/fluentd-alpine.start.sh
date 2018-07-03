0098ae6f15e4f572c7dad44be2d282353f982c19
psagrera <psagrera@juniper.net>
2018-07-03 13:25:43 +0200

    fixing some issues

--- plugins/input-jti/fluentd-alpine.start.sh
+++ plugins/input-jti/fluentd-alpine.start.sh
@@ -2,6 +2,6 @@
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
 
-envtpl --keep-template /fluentd/etc/fluent.conf -o /tmp/fluent.conf
+envtpl --keep-template /fluentd/fluent.conf -o /tmp/fluent.conf
 
 fluentd -c /tmp/fluent.conf -p /fluentd/plugins $FLUENTD_OPT
