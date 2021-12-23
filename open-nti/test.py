3735f333b6f7725a508321b48810742c772615f3
root <root@ocst-2-geo.englab.juniper.net>
2021-12-22 20:39:36 -0800

    evo fix

--- open-nti/test.py
+++ open-nti/test.py
@@ -2,12 +2,12 @@ from jnpr.junos import Device
 from jnpr.junos.utils.start_shell import StartShell
 import re
 
-dev = Device(host='sup-tb7-geodc-1-brackla', user='regress', password='MaRtInI')
+dev = Device(host='sup-tb4-geodc-2-brackla', user='regress', password='MaRtInI')
 
 with StartShell(dev, timeout=60) as ss:
     result = ss.run('top -b -n 1')
     print result[-1]
-    regex = '\s*[0-9]+\s+\w+\s+\d+\s+\d+\s+\d*\S*\s+(\d*\S*)\s+(\d*\S*)\s+\S+\s+(\S+)\s+\S+\s+\S+\s+fibd\r\n'
+    regex = '\s*[0-9]+\s+\w+\s+\d+\s+\d+\s+\d*\S*\s+(\d*\S*)\s+(\d*\S*)\s+\S+\s+(\S+)\s+\S+\s+\S+\s+EvoAftMan'
 
     text_matches = re.search(regex,result[-1],re.MULTILINE)
     print(text_matches.lastindex)
